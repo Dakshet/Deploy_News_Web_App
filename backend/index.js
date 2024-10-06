1ee4dc93902970e1d14047608242095eb418f20d
Dakshet <dakshghole@gmail.com>
2024-10-06 16:15:56 +0530

    Here we added headers.

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use("/uploads", express.static(path.resolve("./uploads")));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', FRONTEND_URL);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    // Respond to preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204); // No content for preflight requests
+    }
+
+    next();
+});
 
 
 // Routes
