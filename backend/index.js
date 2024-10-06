ecdebb8443e5d0f5af827693b1faba0fdac1b239
Dakshet <dakshghole@gmail.com>
2024-10-06 16:17:03 +0530

    Here do changes

--- backend/index.js
+++ backend/index.js
@@ -33,12 +33,6 @@ app.use(cors({
 
 app.options('*', cors()); // Preflight requests handled globally
 
-
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-app.use("/uploads", express.static(path.resolve("./uploads")));
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', FRONTEND_URL);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -52,6 +46,13 @@ app.use((req, res, next) => {
 
     next();
 });
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use("/uploads", express.static(path.resolve("./uploads")));
+
 
 
 // Routes
