d1da5b982c354bc5fc58968be6ab6ea875bd2cc2
Dakshet <dakshghole@gmail.com>
2024-10-07 17:16:43 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -28,12 +28,12 @@ app.use(cors({
 }));
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 
 // MongoDB connection
@@ -42,13 +42,13 @@ app.use((req, res, next) => {
 // })
 
 // Global handling for preflight OPTIONS requests
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', FRONTEND_URL);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(204); // Respond with no content
-});
+// app.options('*', (req, res) => {
+//     res.header('Access-Control-Allow-Origin', FRONTEND_URL);
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.sendStatus(204); // Respond with no content
+// });
 
 
 
