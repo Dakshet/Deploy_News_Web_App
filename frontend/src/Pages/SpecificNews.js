a0d01b195ac0f9731a55d8e2f6b0456ec006c736
Dakshet <dakshghole@gmail.com>
2024-10-11 17:21:02 +0530

    Helmet are use again.

--- frontend/src/Pages/SpecificNews.js
+++ frontend/src/Pages/SpecificNews.js
@@ -3,7 +3,7 @@ import NewsContext from '../Context/News/NewsContext'
 import "./SpecificNews.css"
 import { useLocation, useNavigate } from "react-router-dom"
 import UpdateNews from '../Components/UpdateNews'
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 // Second copy this import
 import {
     FacebookShareButton,
@@ -106,17 +106,17 @@ const SpecificNews = ({ showAddMenu, showAlert, showProfile }) => {
     return (
         <>
             {/* Set dynamic meta tags */}
-            {/* <Helmet>
+            <Helmet>
                 <title>{specificNews.title}</title>
                 <meta property="og:title" content={specificNews.title} />
                 <meta property="og:url" content={shareUrl} />
-                <meta property="og:description" content={specificNews.title} /> */}
-            {/* <meta property="og:image" content={specificNews.coverImageURL || "http://res.cloudinary.com/dpkaxrntd/image/upload/v1727242172/df85jrwmbue1cjymxoc4.jpg"} /> */}
-            {/* <meta property="og:type" content="website" />
+                <meta property="og:description" content={specificNews.title} />
+                {/* <meta property="og:image" content={specificNews.coverImageURL || "http://res.cloudinary.com/dpkaxrntd/image/upload/v1727242172/df85jrwmbue1cjymxoc4.jpg"} /> */}
+                <meta property="og:type" content="website" />
                 <meta property="og:image" content={"http://res.cloudinary.com/dpkaxrntd/image/upload/v1727242172/df85jrwmbue1cjymxoc4.jpg"} />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-            </Helmet> */}
+                {/* <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" /> */}
+            </Helmet>
             {/* {console.log(specificNews.coverImageURL)} */}
 
             <UpdateNews showAlert={showAlert} currentNews={currentNews} updateModal={updateModal} setUpdateModal={setUpdateModal} />
