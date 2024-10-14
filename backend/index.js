178e33d563ec4cb8c6946dc6b86620edbd13539d
Dakshet <dakshghole@gmail.com>
2024-10-14 08:00:19 +0530

    css changes.

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const MONGODB_URL = process.env.MONGODB_URL;
 const FRONTEND_URL = process.env.FRONTEND_URL || "https://www.industrialtimes24.in";
-const PRERENDER_TOKEN = process.env.PRERENDER_TOKEN;
 // const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const PRERENDER_TOKEN = process.env.PRERENDER_TOKEN;
 // console.log(FRONTEND_URL);
 // const FRONTEND_URL = "https://deploy-news-web-frontend.vercel.app";
 
